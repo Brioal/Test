0040436e663995ef539dc0a611d9325c65799ccf
Brioal <brioal@foxmail.com>
2016-08-11 11:32:33 +0800

    Initial commit

---
+++ testmodule/src/main/java/com/brioal/testmodule/BaseActivity.java
@@ -0,0 +1,10 @@
+package com.brioal.testmodule;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Brioal on 2016/8/11.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+}
